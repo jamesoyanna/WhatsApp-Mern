3a0cf0c44949e9822e6e16d08a31b0ac0e8b7fa8
eshoiza <engrjayt200@gmail.com>
2020-09-05 23:35:49 +0100

    added app icons

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import sidebar from './sidebar.css';
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import {Avatar, IconButton } from "@material-ui/core";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import img from '../images/jayman.jpg'
+
 
 const SideBar = () => {
     return (
-        <div className="sidebar">
-            <h1>I am a side bar</h1>
-            
+      <div className="sidebar">
+        <div className="sidebar__header">
+          <Avatar src= {img}/>
+          <div className="sidebar__headerRight">
+            <IconButton>
+              <DonutLargeIcon />
+            </IconButton>
+            <IconButton>
+              <ChatIcon />
+            </IconButton>
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          </div>
         </div>
+      </div>
     );
 }
 
